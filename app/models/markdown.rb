7d8a9898a856ec47fbd788cb9f9224301b05a69a
sugilog <sugilog@gmail.com>
2014-01-02 16:16:41 +0900

    add step0 doc and set link without turbolink

--- app/models/markdown.rb
+++ app/models/markdown.rb
@@ -1,11 +1,19 @@
 require 'redcarpet'
 
 class Markdown
+  class Custom < Redcarpet::Render::HTML
+    include ActionView::Helpers::UrlHelper
+
+    def link(link, title, content)
+      link_to content, link, :title => title, :"data-no-turbolink" => true
+    end
+  end
+
   attr_reader :renderer
 
   def initialize(option = {})
     option.reverse_merge! autolink: true, space_after_headers: true, tables: true
-    @renderer = Redcarpet::Markdown.new Redcarpet::Render::HTML, option
+    @renderer = Redcarpet::Markdown.new Custom, option
   end
 
   def render(text)
--- test/models/markdown_test.rb
+++ test/models/markdown_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class MarkdownTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @markdown = Markdown.new
+  end
+
+  test "render with link should add data-no-turbolink" do
+    link_text = "[sample](/sample/index.html)"
+
+    rendered = @markdown.render link_text
+
+    assert_match /<a\s[^>]*data-no-turbolink[^>]*>sample<\/a>/, rendered
+  end
 end
